717aff2eda44d208835691734e9c8166164cab82
Amadeus Hoffmann <amadeusmaximilian@gmail.com>
2022-09-06 19:12:27 +0200

    added a roll counter and a timer

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import {nanoid} from "nanoid"
 import Confetti from "react-confetti"
 
 function App() {
-  const [dice, setDice] = React.useState(allNewDice())
+    const [dice, setDice] = React.useState(allNewDice())
     const [tenzies, setTenzies] = React.useState(false)
+    const [rolls, setRolls] = React.useState(0)
+    const [startTime, setStartTime] = React.useState(null)
+    const [endTime, setEndTime] = React.useState(null)
+    const [time, setTime] = React.useState(0)
     
     React.useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
@@ -17,6 +21,12 @@ function App() {
             setTenzies(true)
         }
     }, [dice])
+
+    React.useEffect(() => {
+        console.log("you won")
+        tenzies ? end() : start()
+
+    }, [tenzies])
 
     function generateNewDie() {
         return {
@@ -36,6 +46,8 @@ function App() {
     
     function rollDice() {
         if(!tenzies) {
+            setRolls(rolls + 1) // increment rolls
+            
             setDice(oldDice => oldDice.map(die => {
                 return die.isHeld ? 
                     die :
@@ -44,6 +56,7 @@ function App() {
         } else {
             setTenzies(false)
             setDice(allNewDice())
+            setRolls(0)
         }
     }
     
@@ -63,6 +76,28 @@ function App() {
             holdDice={() => holdDice(die.id)}
         />
     ))
+
+    function start() {
+      console.log("start")
+        if (rolls === 0) {
+            console.log("start 2")
+            setStartTime(Date.now())
+        } else {
+            console.log("start 3")
+            return 0
+        }
+    }
+
+    function end() {
+      // Stops the time
+        let timeDiff = Date.now() - startTime; //in ms
+        const seconds = Math.floor(timeDiff / 1000)
+        setTime(seconds)
+    }
+
+    const styles = {
+      backgroundColor: rolls < 10 ? "#9391f1" : "#f15656"
+  } 
     
     return (
         <main>
@@ -72,6 +107,12 @@ function App() {
             Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
+            </div>
+            <div className='dashboard'>
+              <div className='dashboard-text'>Count</div>
+              <div className='rolls-face' style={styles}>{rolls}</div>
+              <div className='time-face'>{time}</div>
+              <div className='dashboard-text'>Time</div>
             </div>
             <button 
                 className="roll-dice" 
